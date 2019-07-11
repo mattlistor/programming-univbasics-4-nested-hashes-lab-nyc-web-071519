2ca1a27b5caeae2e53d03a5f483e509db6817733
Matthew Listor <mattlistor@gmail.com>
2019-07-11 04:18:32 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -125,6 +125,7 @@ def adding_to_dennis
         }
      }
      
-     programmer_hash[:dennis_ritchie][:languages].
+     programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+     return programmer_hash
 
 end
