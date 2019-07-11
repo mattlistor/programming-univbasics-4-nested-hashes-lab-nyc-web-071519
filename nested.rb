b09c185ffecf5f28cb81dae390c67c6be8e1a998
Matthew Listor <mattlistor@gmail.com>
2019-07-11 04:07:30 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     return (programer_hash[:dennis_ritchie][:languages]).join(")
+     return (programer_hash[:dennis_ritchie][:languages]).join("")
 end
 
 def adding_matz
